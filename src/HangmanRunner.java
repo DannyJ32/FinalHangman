882d8f81ae383152d746c344011f9c9e8e7c92f5
Danny <megaslashgt123@gmail.com>
2024-12-09 20:10:43 -0500

    Final

--- src/HangmanRunner.java
+++ src/HangmanRunner.java
@@ -1,4 +1,6 @@
-package PACKAGE_NAME;
-
 public class HangmanRunner {
+    public static void main(String[] args) {
+        HangmanLogic gameLogic = new HangmanLogic();
+        gameLogic.start();
+    }
 }
